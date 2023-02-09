cbacdc2f26de282a7f4159cd67a839fd0134b648
Johnny Wells <johnnyw01github@gmail.com>
2023-02-09 09:45:50 -0600

    Completed the second exercises of the Control Statments and Loops section of Java I.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,32 @@
 public class ControlFlowExercises {
     public static void main (String[] args){
-        int i = 5;
-        while (i <= 15) {
-            System.out.print(i + " ");
-            i++;
-        }
+
+        //#1
+        int count = 0;
+
+        do {
+            System.out.println(count);
+            count += 2;
+        } while (count <= 100);
+
+        //#2
+        int countMinus5 = 100;
+
+        do {
+            System.out.println(countMinus5);
+            countMinus5 -= 5;
+        } while (countMinus5 >= -10);
+
+        //#3
+        long num = 2;
+        System.out.println(num);
+        do {
+            long square = num * num;
+            if (square == 2 || square == 4 || square == 16 || square == 256 || square == 65536) {
+                System.out.println(square);
+            }
+            num++;
+        } while (num * num < 1000000);
 
     }
 }
