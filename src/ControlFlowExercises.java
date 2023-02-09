1f0875878d684b7c6a170d5616fd35c1e58bf2bf
Johnny Wells <johnnyw01github@gmail.com>
2023-02-09 10:33:25 -0600

    Completed the FizzBuzz exercise of the Control Statments and Loops section of Java I.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,28 +1,17 @@
 public class ControlFlowExercises {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        // While-loop as a For-loop
-        for(int i = 5; i <= 15; i++){
-            System.out.print(i + " ");
+        //#2 FizzBuzz game:
+        for(int i = 1; i <= 100; i++ ){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }else if(i % 3 == 0){
+                System.out.println("Fizz");
+            } else if (i % 5 == 0 ) {
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
         }
-
-        //Do-while-loop as a For-loop
-        //#1
-        for(int count = 0; count <= 100; count +=2){
-            System.out.println(count);
-        }
-
-
-        //#2
-        for(int countMinus5 = 100; countMinus5 >= -10; countMinus5 -= 5 ){
-            System.out.println(countMinus5);
-        }
-
-
-        //#3
-        for(long i = 2; i < 1000000; i = i * i){
-            System.out.println(i);
-        }
-
     }
 }
