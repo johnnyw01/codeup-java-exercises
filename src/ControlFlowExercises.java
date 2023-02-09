f471bfca95c0be9483b101acf67eef3ff6fbc591
Johnny Wells <johnnyw01github@gmail.com>
2023-02-09 11:41:43 -0600

    Completed the Display Powers Table exercise of the Control Statments and Loops section of Java I.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,17 +1,25 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        //#2 FizzBuzz game:
-        for(int i = 1; i <= 100; i++ ){
-            if(i % 3 == 0 && i % 5 == 0){
-                System.out.println("FizzBuzz");
-            }else if(i % 3 == 0){
-                System.out.println("Fizz");
-            } else if (i % 5 == 0 ) {
-                System.out.println("Buzz");
-            }else{
-                System.out.println(i);
+        //#3  Display a table of powers:
+
+        Scanner scanner = new Scanner(System.in);
+        String askAgain;
+        do {
+            System.out.println("Please enter a number to see a table showing the powers of that number?");
+            int userNumber = scanner.nextInt();
+            System.out.println("Here is your table!");
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
+            for (int i = 1; i <= userNumber; i++) {
+                System.out.println(String.format("%-6d | %-7d | %-6d", i, i * i, i * i * i));
             }
-        }
+            System.out.print("Do you want to continue? (y/n): ");
+//            scanner.nextLine();
+            askAgain = scanner.next();
+        } while (askAgain.equals("y"));
+        System.out.println("Goodbye!");
     }
 }
